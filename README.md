37babdcc7cfc998d6b376de88b397a6ae3a0b3c9
marzelop <maagyo.teo23@gmail.com>
2024-03-10 20:38:03 -0300

    Add resource files (Icons)

---
+++ README.md
@@ -0,0 +1,4 @@
+Ícones Utilizados/Icons Used:
+[Cursor icons created by Freepik - Flaticon]("https://www.flaticon.com/free-icons/cursor")
+[Stop icons created by Freepik - Flaticon]("https://www.flaticon.com/free-icons/stop")
+[Straight icons created by Flipicon - Flaticon]("https://www.flaticon.com/free-icons/straight")
--- app.py
+++ app.py
@@ -1,40 +1,189 @@
 #!./venv/bin/python
-from PySide6.QtCore import Qt
-from PySide6.QtGui import QFont
+from PySide6.QtCore import Qt, QLine, QPointF
+from PySide6.QtGui import QFont, QPainter, QColor, QPen, QPainterPath, QAction
 from PySide6.QtWidgets import (
 	QLabel, QMainWindow, QApplication, QWidget,
-	QVBoxLayout, QPushButton, QGridLayout
+	QVBoxLayout, QHBoxLayout, QPushButton, QGridLayout,
+	QGraphicsView, QGraphicsScene, QGraphicsEllipseItem,
+	QGraphicsItem, QGraphicsPathItem, QListWidget,
+	QListWidgetItem, QMenuBar, QMenu, QToolBar
 )
 import networkx as nx
 import network as net
+import random
 
-class Node(QLabel):
-	def __init__(self, text: str):
-		super().__init__()
-		self.setText(text)
-		#self.setStyleSheet("border: 2px solid black; border-radius: 50%;")
+rad = 5
+
+class WindowClass(QMainWindow):
+	def __init__(self):
+		super(WindowClass, self).__init__()
+		self.setWindowTitle("NIEP GUI")
+		self.mainWidget = MainWidget()
+		self.menuBar = self.createMenuBar()
+
+		self.setMenuBar(self.menuBar)
+		self.setCentralWidget(self.mainWidget)
+	
+	def createMenuBar(self) -> QMenuBar:
+		menuBar = QMenuBar()
+		menus = {
+			"&File": {
+				"New": None,
+				"Load": None,
+				"Save": None,
+				"Save as ...": None,
+				"Export as ...": None
+			},
+			"&Help": {
+				"Documentation": None,
+				"Report a bug": None,
+				"Troubleshooting": None
+			}
+		}
+
+		for menu in menus.keys():
+			newmenu = QMenu(menu)
+			for action in menus[menu].keys():
+				newmenu.addAction(action)
+			menuBar.addMenu(newmenu)
+
+		return menuBar
+
+
+class MainWidget(QWidget):
+	def __init__(self):
+		super(MainWidget, self).__init__()
+		layout = QVBoxLayout()
+		layout.addWidget(TopMenu())
+		layout.addWidget(EVWrapper())
+		self.setLayout(layout)
+
+
+class TopMenu(QToolBar):
+	def __init__(self):
+		super(TopMenu, self).__init__()
+		actions = {
+			"&Test1": self.testf,
+			"&Test2": self.testf,
+			"&Test3": self.testf
+		}
+		
+		for action in actions.keys():
+			newAction = QAction(action)
+			self.addAction(newAction)
+
+	def testf(self):
+		print(self.sender().text())
 		
 
-class AppMainWindow(QMainWindow):
+class EVWrapper(QWidget):
+	'''
+	Class to encapsulate the EditMenu and ViewClass into one widget.
+	'''
 	def __init__(self):
-		super().__init__()
-		self.netG = net.loadTopologyGraph("ServerClient.json")
-		font = QFont()
-		font.setPixelSize(30)
-		base = QWidget()
-		base.setFont(font)
-		layout = QGridLayout()
-		positions = [(0, 0), (1, 1), (0, 2)]
-		for i, node in enumerate(list(self.netG.nodes)):
-			layout.addWidget(Node(node), positions[i][0], positions[i][1])
-			print(node)
-		base.setLayout(layout)
-		self.setCentralWidget(base)
+		super(EVWrapper, self).__init__()
+		layout = QHBoxLayout()
+		layout.addWidget(EditMenu())
+		layout.addWidget(ViewClass())
+		self.setLayout(layout)
+
+
+class EditMenu(QWidget):
+	def __init__(self):
+		super(EditMenu, self).__init__()
+		layout = QVBoxLayout()
+		self.options = QListWidget()
+		QListWidgetItem("test1", self.options)
+		QListWidgetItem("test2", self.options)
+		QListWidgetItem("test3", self.options)
+		layout.addWidget(self.options)
+		self.setLayout(layout)
+
+
+class ViewClass(QGraphicsView):
+	def __init__(self):
+		super(ViewClass, self).__init__()
+
+		self.setDragMode(QGraphicsView.RubberBandDrag)
+		# self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+		# self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+
+		self.s = SceneClass()
+		self.setScene(self.s)
+		self.setRenderHint(QPainter.Antialiasing)
+
+
+class SceneClass(QGraphicsScene):
+	def __init__(self, id=None):
+		super(SceneClass, self).__init__()
+		self.setSceneRect(-500, -500, 1000, 1000)
+		self.grid = 25
+		self.it = None
+		self.node = None
+
+	def drawBackground(self, painter, rect):
+		if False:
+			painter = QPainter()
+			rect = QRect()
+
+		painter.fillRect(rect, QColor(30, 30, 30))
+		left = int(rect.left()) - int((rect.left()) % self.grid)
+		top = int(rect.top()) - int((rect.top()) % self.grid)
+		right = int(rect.right())
+		bottom = int(rect.bottom())
+		lines = []
+		for x in range(left, right, self.grid):
+			lines.append(QLine(x, top, x, bottom))
+		for y in range(top, bottom, self.grid):
+			lines.append(QLine(left, y, right, y))
+		painter.setPen(QPen(QColor(50, 50, 50)))
+		painter.drawLines(lines)
+
+	def mousePressEvent(self, event):
+		if event.button() == Qt.RightButton:
+			path = QPainterPath()
+			path.moveTo(event.scenePos() + QPointF(random.randint(-100, 100), random.randint(-100, 100)))
+			path.lineTo(event.scenePos())
+			self.addItem(Path(path, self))
+		super(SceneClass, self).mousePressEvent(event)
+
+class Node(QGraphicsEllipseItem):
+	def __init__(self, path, index):
+		super(Node, self).__init__(-rad, -rad, 2*rad, 2*rad)
+
+		self.rad = rad
+		self.path = path
+		self.index = index
+
+		self.setZValue(1)
+		self.setFlag(QGraphicsItem.ItemIsMovable)
+		self.setFlag(QGraphicsItem.ItemSendsGeometryChanges)
+		self.setBrush(Qt.green)
+
+	def itemChange(self, change, value):
+		if change == QGraphicsItem.ItemPositionChange:
+			self.path.updateElement(self.index, value)
+		return QGraphicsEllipseItem.itemChange(self, change, value)
+
+
+class Path(QGraphicsPathItem):
+	def __init__(self, path, scene):
+		super(Path, self).__init__(path)
+		for i in range(path.elementCount()):
+			node = Node(self, i)
+			node.setPos(QPointF(path.elementAt(i)))
+			scene.addItem(node)
+		self.setPen(QPen(Qt.red, 1.75))        
+
+	def updateElement(self, index, pos):
+		path = self.path()
+		path.setElementPositionAt(index, pos.x(), pos.y())
+		self.setPath(path)
 
 
 app = QApplication()
 
-window = AppMainWindow()
+window = WindowClass()
 window.show()
 
 app.exec()
