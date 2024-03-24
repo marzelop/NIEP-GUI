6b7276266510421601dc98280051c9fb5bfb484e
marzelop <maagyo.teo23@gmail.com>
2024-03-24 11:12:58 -0300

    Remove useless and commented code

--- app.py
+++ app.py
@@ -113,8 +113,6 @@ class ViewClass(QGraphicsView):
 		super(ViewClass, self).__init__()
 
 		self.setDragMode(QGraphicsView.RubberBandDrag)
-		# self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-		# self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
 
 		self.s = SceneClass()
 		self.setScene(self.s)
@@ -138,10 +136,6 @@ class SceneClass(QGraphicsScene):
 		self.netgraph = nx.Graph()
 
 	def drawBackground(self, painter, rect):
-		if False:
-			painter = QPainter()
-			rect = QRect()
-
 		painter.fillRect(rect, QColor(210, 210, 210))
 		left = int(rect.left()) - int((rect.left()) % self.grid)
 		top = int(rect.top()) - int((rect.top()) % self.grid)
@@ -157,11 +151,6 @@ class SceneClass(QGraphicsScene):
 
 	def mousePressEvent(self, event):
 		if event.button() == Qt.RightButton:
-			# path = QPainterPath()
-			# path.moveTo(event.scenePos() + QPointF(random.randint(-100, 100), random.randint(-100, 100)))
-			# path.lineTo(event.scenePos())
-			# self.addItem(Path(path, self))
-
 			node = Node("Cliente1")
 			node.setPos(event.scenePos())
 			self.addItem(node)
@@ -183,24 +172,6 @@ class SceneClass(QGraphicsScene):
 		u.edges.append(edge)
 		v.edges.append(edge)
 		self.addItem(edge)
-
-# class Node(QGraphicsEllipseItem):
-# 	def __init__(self, path, index):
-# 		super(Node, self).__init__(-rad, -rad, 2*rad, 2*rad)
-# 
-# 		self.rad = rad
-# 		self.path = path
-# 		self.index = index
-# 
-# 		self.setZValue(1)
-# 		self.setFlag(QGraphicsItem.ItemIsMovable)
-# 		self.setFlag(QGraphicsItem.ItemSendsGeometryChanges)
-# 		self.setBrush(Qt.green)
-# 
-# 	def itemChange(self, change, value):
-# 		if change == QGraphicsItem.ItemPositionChange:
-# 			self.path.updateElement(self.index, value)
-# 		return QGraphicsEllipseItem.itemChange(self, change, value)
 
 
 class Node(QGraphicsEllipseItem):
@@ -227,17 +198,6 @@ class Node(QGraphicsEllipseItem):
 	def getName(self) -> str:
 		return self.text.toPlainText()
 
-	def connectTo(self, node: Node):
-		self.edges += Edge(self, node)
-
-	def itemChange(self, change, value):
-		print(self.i)
-		self.i+=1
-		if change == QGraphicsItem.ItemPositionChange:
-			print(self.edges)
-			for edge in self.edges:
-				edge.updateLine()
-		return super().itemChange(change, value)
 	
 
 class Edge(QGraphicsLineItem):
